66e9f26f5958c101902923a6d86247f9c43899f3
Philipp Mundhenk <philipp@mundhenk.org>
2016-03-04 23:12:05 +0800

    limited parameters to toc tag

--- site/tags/sections.php
+++ site/tags/sections.php
@@ -2,15 +2,42 @@
 
 kirbytext::$tags['l1'] = array(
   'attr' => array(
-    'split',
-    'lowercase'
   ),
   'html' => function($tag) {
-  	$link = $tag->attr('l1');
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
+   	$link = $tag->attr('l1');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h1><a name="' . $link . '">'. $tag->attr('l1') . '</a></h1>';
@@ -23,11 +50,40 @@ kirbytext::$tags['l2'] = array(
     'lowercase'
   ),
   'html' => function($tag) {
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
   	$link = $tag->attr('l2');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h2><a name="' . $link . '">'. $tag->attr('l2') . '</a></h2>';
@@ -40,11 +96,40 @@ kirbytext::$tags['l3'] = array(
     'lowercase'
   ),
   'html' => function($tag) {
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
   	$link = $tag->attr('l3');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h3><a name="' . $link . '">'. $tag->attr('l3') . '</a></h3>';
@@ -57,11 +142,40 @@ kirbytext::$tags['l4'] = array(
     'lowercase'
   ),
   'html' => function($tag) {
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
   	$link = $tag->attr('l4');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h4><a name="' . $link . '">'. $tag->attr('l4') . '</a></h4>';
@@ -74,11 +188,40 @@ kirbytext::$tags['l5'] = array(
     'lowercase'
   ),
   'html' => function($tag) {
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
   	$link = $tag->attr('l5');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h5><a name="' . $link . '">'. $tag->attr('l5') . '</a></h5>';
@@ -91,11 +234,40 @@ kirbytext::$tags['l6'] = array(
     'lowercase'
   ),
   'html' => function($tag) {
+
+  	foreach($tag->page()->content() as $link) {
+	  $cnt+=1;
+      if($cnt==3) {
+		preg_match_all("/\(toc: .*\)/", $link, $tocs);
+		foreach ($tocs[0] as $toc) {
+			preg_match_all("/lowercase:(\ )?[0,1]?/", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/lowercase:/", "", $param);
+				$param = preg_replace("/\ /", "", $param);
+				if($param==1)
+				{
+					$lowercase=1;
+				}
+				else
+				{
+					$lowercase=0;
+				}
+			}
+			$splitchar = '%20';
+			preg_match_all("/split:(\ )?./", $toc, $parameters);
+			foreach ($parameters[0] as $param) {
+				$param = preg_replace("/split:(\ )?/", "", $param);
+				$param = preg_replace("/\ ./", "", $param);
+				$splitchar = $param;
+			}
+		}
+	  }
+    }
+
   	$link = $tag->attr('l6');
   	$link = preg_replace("/[^ \w]+/", "", $link);
-  	$splitchar = $tag->attr('split', "%20");
   	$link = str_replace(" ", $splitchar, $link);
-  	if($tag->attr('lowercase')==1) {
+  	if($lowercase==1) {
 		$link = strtolower($link);
 	}
     return '<h6><a name="' . $link . '">'. $tag->attr('l6') . '</a></h6>';
@@ -125,19 +297,18 @@ kirbytext::$tags['toc'] = array(
 			$name = preg_split("/\(l[1-6]?: /",$heading);
 			$name = preg_split("/\)/",$name[1]);
 			$link = $name[0];
-			$link = preg_replace("/\ split: ./", "", $link);
-			$link = preg_replace("/\ lowercase: \d/", "", $link);
-			// $link = normalizer_normalize( $name[0], Normalizer::FORM_C );
+
 			$link = preg_replace("/[^ \w]+/", "", $link);
 			$link = str_replace(" ", $splitchar, $link);
 			$link = htmlentities($link);
-			if($tag->attr('lowercase')==1) {
-				$link = strtolower($link);
-			}
 
 			$number = preg_split("/\(l/",$heading);
 			$number = preg_split("/: .*\)/",$number[1]);
 			$number = $number[0];
+
+			if($tag->attr('lowercase')==1) {
+				$link = strtolower($link);
+			}
 
 			if($number <= $tag->attr('toc'))
 			{
@@ -145,8 +316,8 @@ kirbytext::$tags['toc'] = array(
 				  $html .= $levelchar.' ';
 				}
 
-				$name = preg_replace("/\ split: ./", "", $name[0]);
-				$name = preg_replace("/\ lowercase: \d/", "", $name);
+				$name = preg_replace("/\ split:(\ )?./", "", $name[0]);
+				$name = preg_replace("/\ lowercase:(\ )?\d/", "", $name);
 
 				$html .= '<a href="#';
 				$html .= $link;
